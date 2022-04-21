51ae8066705f43c3763fdf912b25f24ea082cb1d
Adrian Lugo <misteralugo@gmail.com>
2022-04-21 17:33:38 -0500

    added state for all form values & can log them out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
 import { Paper, TextField, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, InputLabel, Select, MenuItem, Button, Typography } from '@mui/material'
 import CalculateIcon from '@mui/icons-material/Calculate';
 
 
 function App() {
+  const [formValues, setFormValues] = React.useState({
+    age: undefined,
+    gender: undefined,
+    feet: undefined,
+    inches: undefined,
+    pounds: undefined,
+    activity: "1.20"
+  })
+
+  function handleInputChange(e) {
+    const { name, value } = e.target
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  function calculate() {
+    console.log(formValues)
+  }
+
   return (
     <div className="app">
-      <Paper elevation={24 } id="paper">
+      <Paper elevation={24} id="paper">
         <form id="form">
           <Typography id="title" variant='h3'>
             Calorie Calculator
           </Typography>
           {/* Age */}
           <FormLabel id="age-label">Age</FormLabel>
-          <TextField id="age-input" label="age" helperText="Ages 15-80" />
+          <TextField id="age-input" label="age" helperText="Ages 15-80" onChange={handleInputChange} name="age" />
           {/* Gender */}
           <FormLabel id="gender-label">Gender</FormLabel>
           <FormControl id="gender-form">
             <RadioGroup
               aria-labelledby="gender-label"
-              name="radio-buttons-group"
+              name="gender"
               row
+              onChange={handleInputChange}
             >
               <FormControlLabel value="male" control={<Radio />} label="Male" />
               <FormControlLabel value="female" control={<Radio />} label="Female" />
@@ -28,12 +51,12 @@ function App() {
           {/* Height */}
           <FormLabel id="height-label">Height</FormLabel>
           <div className="height-inputs">
-            <TextField id="input-feet" label="feet"  />
-            <TextField id="input-inches" label="inches" />
+            <TextField id="input-feet" label="feet" onChange={handleInputChange} name="feet" />
+            <TextField id="input-inches" label="inches" onChange={handleInputChange} name="inches" />
           </div>
           {/* Weight */}
           <FormLabel id="weight-label">Weight</FormLabel>
-          <TextField id="weight-input" label="pounds" />
+          <TextField id="weight-input" label="pounds" onChange={handleInputChange} name="pounds" />
           {/* Activity */}
           <FormLabel id="activity-label">Activity</FormLabel>
           <FormControl fullWidth>
@@ -41,20 +64,21 @@ function App() {
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              // value={age}
+              value={formValues.activity}
               label="select activity level"
-              // onChange={handleChange}
+              onChange={handleInputChange}
+              name="activity"
             >
-              <MenuItem value={1.00}>BMR</MenuItem>
-              <MenuItem value={1.20}>Sedentary</MenuItem>
-              <MenuItem value={1.37}>Light</MenuItem>
-              <MenuItem value={1.46}>Moderate</MenuItem>
-              <MenuItem value={1.55}>Active</MenuItem>
-              <MenuItem value={1.73}>Very Active</MenuItem>
-              <MenuItem value={1.90}>Extra Active</MenuItem>
+              <MenuItem value={"1.00"}>BMR</MenuItem>
+              <MenuItem value={"1.20"}>Sedentary</MenuItem>
+              <MenuItem value={"1.37"}>Light</MenuItem>
+              <MenuItem value={"1.46"}>Moderate</MenuItem>
+              <MenuItem value={"1.55"}>Active</MenuItem>
+              <MenuItem value={"1.73"}>Very Active</MenuItem>
+              <MenuItem value={"1.90"}>Extra Active</MenuItem>
             </Select>
           </FormControl>
-          <Button id="calculate" variant="contained" endIcon={<CalculateIcon />}>
+          <Button id="calculate" variant="contained" endIcon={<CalculateIcon />} onClick={calculate}>
             Calculate
           </Button>
         </form>
